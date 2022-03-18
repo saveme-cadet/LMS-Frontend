3fbd93fd9891fa0d3ca89ac845a114e1f2318d41
GulSam00 <gulsamcono@gmail.com>
2022-03-18 20:56:37 +0900

    Feat : Feat : 아오지 네트워크 모듈 추가.

--- src/Network/index.js
+++ src/Network/index.js
@@ -1,6 +1,7 @@
 import UserInfoService from './UserInfoService';
 import AllTableService from './AllTableService';
 import TodoService from './TodoService';
+import AojiService from './AojiService';
 import CRUDUserService from './CRUDUserService';
 
-export { UserInfoService, AllTableService, TodoService, CRUDUserService };
+export { UserInfoService, AllTableService, TodoService, AojiService, CRUDUserService };
