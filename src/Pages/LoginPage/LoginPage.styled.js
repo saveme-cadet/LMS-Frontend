615780bb9f7955991a7271dcb7117254e31b2e43
luckylooky2 <luckylooky2@naver.com>
2022-04-04 22:38:05 +0900

    Feat. 로그인 디자인 변경, 비밀번호 숨기기 추가

--- src/Pages/LoginPage/LoginPage.styled.js
+++ src/Pages/LoginPage/LoginPage.styled.js
@@ -8,36 +8,125 @@ const LoginBackground = styled.div`
 
   text-align: center;
   color: white;
-  background-color: #220646;
+  background-image: url('/asset/login.jpg'); no-repeat;
+  background-size: cover;
+  // background-color: #220646;
+
   // size
   width: 100%;
   height: 100%;
 
+  @font-face {
+    font-family: 'BMJUA';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMJUA.woff')
+      format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  .main {
+    margin-top: 5%;
+  }
+
   .title {
-    font-size: 50px;
+    font-size: 70px;
     margin: 20px;
+    font-family: 'BMJUA';
   }
 
-  .id::before {
-    content: '아이디 :  ';
+  .id {
+    margin-top: 5%;
+  }
+  .idinput {
+    padding-left : 10px;
+    border-color : transparent;
+    border-radius: 0.3em;
+    height: 50px;
+    width: 490px;
+    font-size: 20px;
+    font-family: 'BMJUA';
   }
 
-  .email::before {
+  .emailinput {
     content: '이메일 : ';
   }
-  .password::before {
-    content: '비밀번호 : ';
-  }
-  .password {
-    padding-right: 15px;
-  }
-  button {
+
+  .passwordinput {
+    padding-left : 10px;
+    border-color : transparent;
+    border-radius: 0.3em;
+    margin-top: 1%;
+    height: 50px;
+    width: 490px;
     font-size: 20px;
-    width: 20%;
+    font-family: 'BMJUA';
+  }
+
+  .loginbutton {
+    border-radius: 0.3em;
+    font-family: 'BMJUA';
+    font-size: 20px;
+    color : white;
+    width: 510px;
     height: 2.5em;
     margin: 1em;
     //  cursor: pointer;
-    background-color: white;
+    background-color: #00AAFF;
+  }
+
+  .registerbutton {
+    margin-top : 5%;
+    font-family: 'BMJUA';
+    font-size: 20px;
+    text-decoration: underline;
+    color : white;
+    width: 500px;
+  }
+
+  .registerform {
+    margin-top : 3%;
+    width : 550px;
+    height : 70%;
+    background-color : #FFFFFF;
+    border-radius: 1em;
+  }
+
+  .welcome {
+    margin-top : 5%;
+    color : black;
+    font-family: 'BMJUA';
+    font-size : 25px;
+  }
+
+  .registeridinput {
+    border: 2px solid #868A8C;
+    margin-top : 5%;
+    padding-left : 10px;
+    border-radius: 0.3em;
+    height: 50px;
+    width: 490px;
+    font-size: 20px;
+    font-family: 'BMJUA';
+  }
+
+  .registerpasswordinput {
+    border: 2px solid #868A8C;
+    margin-top : 2%;
+    padding-left : 10px;
+    border-radius: 0.3em;
+    height: 50px;
+    width: 490px;
+    font-size: 20px;
+    font-family: 'BMJUA';
+  }
+
+  .backbutton {
+    margin-top : 15%;
+    font-family: 'BMJUA';
+    font-size: 20px;
+    text-decoration: underline;
+    color : #000000;
+    width: 500px;
   }
 `;
 
