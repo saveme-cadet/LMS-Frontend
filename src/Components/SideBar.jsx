973bde73fb8795899344123d8977293cc1296094
GulSam00 <gulsamcono@gmail.com>
2022-08-23 23:27:53 +0900

    Feat : 게더타운 링크 추가

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -8,6 +8,8 @@ import Tab from '@mui/material/Tab';
 
 import styled from 'styled-components';
 
+const gatherTownLink = 'https://app.gather.town/app/Zq3peLuvz5isVQ0f/42seoul';
+
 const SideBar = () => {
   const [curPage, setCurPage] = useState(null);
   const navi = useNavigate();
@@ -19,7 +21,7 @@ const SideBar = () => {
     navi(`/${value}`);
   };
   const handleClickLogout = async () => {
-    const result = CRUDUserService.postLogout();
+    const result = await CRUDUserService.postLogout();
     localStorage.clear();
     auth.setStatus(null);
   };
@@ -31,7 +33,7 @@ const SideBar = () => {
   }, []);
 
   return (
-    <>
+    <SideBarContainer>
       <HomeButton
         onClick={() => {
           navi('/');
@@ -52,6 +54,12 @@ const SideBar = () => {
           <Tab className="button" label="오늘 할 일" value="todo" />
           <Tab className="button" label="아오지 탄광" value="mine" />
           <Tab className="button" label="머슴" value="admin" />
+          <div
+            className="button link"
+            onClick={() => window.open(gatherTownLink, '_blank')}
+          >
+            게더타운 바로가기
+          </div>
           <Tab
             className="button logout"
             label="로그아웃"
@@ -59,11 +67,57 @@ const SideBar = () => {
           />
         </Tabs>
       )}
-    </>
+    </SideBarContainer>
   );
 };
 
 export default SideBar;
+
+const SideBarContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  background-color: #220646;
+  height: 100%;
+  width: 150px;
+
+  .tabs {
+    height: 100%;
+  }
+  .sub-tab {
+    display: flex;
+    flex-direction: row;
+    position: absolute;
+    bottom: 0;
+  }
+
+  .button {
+    color: #ffffff;
+    max-width: 13em;
+  }
+  .link {
+    position: absolute;
+    bottom: 7rem;
+    width: 100%;
+    text-align: center;
+
+    cursor: pointer;
+  }
+  .logout {
+    text-align: center;
+    border: 1px solid transparent;
+    background-color: #110323;
+    cursor: pointer;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 5rem;
+    font-size: 20px;
+    font-weight: bold;
+  }
+`;
 
 const HomeButton = styled.div`
   margin: 10px 0 20px;
--- src/Route.jsx
+++ src/Route.jsx
@@ -7,9 +7,7 @@ import styled from 'styled-components';
 const MainRoute = () => {
   return (
     <>
-      <SideBarContainer>
-        <SideBar />
-      </SideBarContainer>
+      <SideBar />
       <BodyContainer>
         <Routes>
           <Route path="/" element={<MainPage />} />
@@ -26,37 +24,6 @@ const MainRoute = () => {
 };
 
 export default MainRoute;
-
-const SideBarContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  display: flex;
-  flex-direction: column;
-  background-color: #220646;
-  height: 100%;
-  width: 150px;
-
-  .tabs {
-    height: 100%;
-  }
-  .button {
-    color: #ffffff;
-    max-width: 13em;
-  }
-  .logout {
-    text-align: center;
-    border: 1px solid transparent;
-    background-color: #110323;
-    cursor: pointer;
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 5rem;
-    font-size: 20px;
-    font-weight: bold;
-  }
-`;
 
 const BodyContainer = styled.div`
   margin-left: 150px;
