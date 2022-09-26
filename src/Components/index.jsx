e552eb93c1c1e90a10fe6b3b7657f4382c6b084b
mike2ox <firefinger07@gmail.com>
2022-09-26 21:26:48 +0900

    Chore : add ShowDay component in export

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,7 +1,16 @@
 import SideBar from './SideBar';
 import CusDatePicker from './CusDatePicker';
 import ShowToday from './ShowToday';
+import ShowDay from './ShowDay';
 import GetCheckIcons from './GetCheckIcons';
 import NotValid from './NotValid';
 import NoData from './NoData';
-export { SideBar, CusDatePicker, ShowToday, GetCheckIcons, NotValid, NoData };
+export {
+  SideBar,
+  CusDatePicker,
+  ShowToday,
+  GetCheckIcons,
+  NotValid,
+  NoData,
+  ShowDay,
+};
